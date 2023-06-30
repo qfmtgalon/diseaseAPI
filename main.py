d8e24edfb91c3d51c887a1ff9ab2eded48d488d7
Faith Mary Galon <68683084+qfmtgalon@users.noreply.github.com>
2023-06-30 16:21:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+
+
+
+Hi! Here some our recommendations to get the best out of BLACKBOX:
+
+Be as clear as possible
+
+End the question in what language you want the answer to be, e.g: ‘connect to mongodb in python
+
+You can manage your extension preferences by from here
+or you can just
+Watch tutorial video
+Here are some suggestion (choose one):
+Write a function that reads data from a json file
+How to delete docs from mongodb in phyton
+Connect to mongodb in nodejs
+Ask any coding question
+send
+refresh
+Blackbox AI Chat is in beta and Blackbox is not liable for the content generated. By using Blackbox, you acknowledge that you agree to agree to Blackbox's Terms and Privacy Policy
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -6,22 +26,16 @@ app = FastAPI()
 class Msg(BaseModel):
     msg: str
 
-
-@app.get("/")
+@app.get("/anthracnose")
 async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
+    return {"DESCRIPTION": "Antracnose is a fungal disease that affects a wide range of plants, including mango trees. It is caused by several species of fungi in the Colletotrichum genus, which can infect the leaves, stems, flowers, and fruit of the tree. Symptoms of antracnose on mango trees include small, dark-colored spots on the leaves, stems, and fruit.", "LETHALITY": "Not lethal, moderate", "HUMIDITY": "Warm and moist weather"}
 
 
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
+@app.get("/sootymold")
+async def root():
+    return {"DESCRIPTION": "Dark Brown, Black mostly seen in Warm and Dry period and  grows on honeydew excreted by piercing sucking insects.", "LETHALITY": "Not lethal, Mild, Moderate", "HUMIDITY": "Warm or Dry Weather."}
 
 
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+@app.get("/redrust")
+async def root():
+    return {"DESCRIPTION": "Red rust is a fungal disease that damages mango plants. Puccinia mangiferae affects the tree's leaves. Reddish-brown or orange pustules on the leaves may cause distortion and premature leaf drop. This reduces tree growth and fruit yield.", "LETHALITY": "Not lethal, Mild, Moderate", "HUMIDITY": "Wet and warm weather"}
